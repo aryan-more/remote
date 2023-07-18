4e0a14caff23c7d013eb299fcc68a07cc3d8fa85
aryan-more <morearyan@outlook.com>
2023-07-18 14:46:55 +0530

    Added option for ago

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -14,6 +14,34 @@ extension DateTimeExt on DateTime {
   String get dmy => "${_addZero(day)}-${_addZero(month)}-$year";
 
   String get dateTime => DateFormat.yMMMd().add_jm().format(this);
+
+  String get ago {
+    final now = DateTime.now();
+    final difference = now.difference(this);
+
+    if (difference.inDays > 365) {
+      final years = (difference.inDays / 365).floor();
+      return '$years ${years == 1 ? 'year' : 'years'} ago';
+    }
+
+    if (difference.inDays > 30) {
+      final months = (difference.inDays / 30).floor();
+      return '$months ${months == 1 ? 'month' : 'months'} ago';
+    }
+
+    if (difference.inDays > 0) {
+      return '${difference.inDays} ${difference.inDays == 1 ? 'day' : 'days'} ago';
+    }
+
+    if (difference.inHours > 0) {
+      return '${difference.inHours} ${difference.inHours == 1 ? 'hour' : 'hours'} ago';
+    }
+
+    if (difference.inMinutes > 0) {
+      return '${difference.inMinutes} ${difference.inMinutes == 1 ? 'minute' : 'minutes'} ago';
+    }
+    return 'Just now';
+  }
 }
 
 class EdgeInsetsEXT {
