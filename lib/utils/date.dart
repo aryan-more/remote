d93923327d1e8f8135292795c3aed91167643c92
aryan-more <morearyan@outlook.com>
2023-09-11 17:08:07 +0530

    Added Custom date range for start and end date

--- lib/utils/date.dart
+++ lib/utils/date.dart
@@ -5,12 +5,18 @@ mixin DateRange {
   final Rxn<DateTime> start = Rxn();
   final Rxn<DateTime> end = Rxn();
 
+  DateTime get startFirstDate => DateTime(2023, 4, 1);
+  DateTime get startFirstEnd => end.value ?? DateTime.now().add(const Duration(days: 365 * 10));
+
+  DateTime get endFirstDate => start.value ?? DateTime(2023, 4, 1);
+  DateTime get endFirstEnd => DateTime.now().add(const Duration(days: 365 * 10));
+
   void pickStartDate() async {
     DateTime? date = await showDatePicker(
       context: Get.context!,
       initialDate: start.value ?? DateTime.now(),
-      firstDate: DateTime(2023, 4, 1),
-      lastDate: end.value ?? DateTime.now().add(const Duration(days: 365 * 10)),
+      firstDate: startFirstDate,
+      lastDate: startFirstEnd,
     );
     if (date != null && date != start.value) {
       start.value = date;
@@ -22,8 +28,8 @@ mixin DateRange {
     DateTime? date = await showDatePicker(
       context: Get.context!,
       initialDate: end.value ?? DateTime.now(),
-      firstDate: start.value ?? DateTime(2023, 4, 1),
-      lastDate: DateTime.now().add(const Duration(days: 365 * 10)),
+      firstDate: endFirstDate,
+      lastDate: endFirstEnd,
     );
     if (date != null && date != end.value) {
       end.value = date;
