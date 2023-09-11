5bc976e1a18ba8924a66e485c5b2c90273d2f6d1
aryan-more <morearyan@outlook.com>
2023-09-11 17:13:38 +0530

    Fixed Typos

--- lib/utils/date.dart
+++ lib/utils/date.dart
@@ -6,17 +6,17 @@ mixin DateRange {
   final Rxn<DateTime> end = Rxn();
 
   DateTime get startFirstDate => DateTime(2023, 4, 1);
-  DateTime get startFirstEnd => end.value ?? DateTime.now().add(const Duration(days: 365 * 10));
+  DateTime get startLastDate => end.value ?? DateTime.now().add(const Duration(days: 365 * 10));
 
   DateTime get endFirstDate => start.value ?? DateTime(2023, 4, 1);
-  DateTime get endFirstEnd => DateTime.now().add(const Duration(days: 365 * 10));
+  DateTime get endLastDate => DateTime.now().add(const Duration(days: 365 * 10));
 
   void pickStartDate() async {
     DateTime? date = await showDatePicker(
       context: Get.context!,
       initialDate: start.value ?? DateTime.now(),
       firstDate: startFirstDate,
-      lastDate: startFirstEnd,
+      lastDate: startLastDate,
     );
     if (date != null && date != start.value) {
       start.value = date;
@@ -29,7 +29,7 @@ mixin DateRange {
       context: Get.context!,
       initialDate: end.value ?? DateTime.now(),
       firstDate: endFirstDate,
-      lastDate: endFirstEnd,
+      lastDate: endLastDate,
     );
     if (date != null && date != end.value) {
       end.value = date;
