da08ffa0ad1ba86b8c5928dfe694554dfb17cc07
aryan-more <morearyan@outlook.com>
2023-10-18 15:47:11 +0530

    Fixed Bug where mutex isn't released

--- lib/remote/task.dart
+++ lib/remote/task.dart
@@ -24,54 +24,53 @@ abstract class RemoteTask extends LocalTask {
     if (_mutex.isLocked) {
       return false;
     }
-    await _mutex.acquire();
-    String? error;
-    try {
-      if (!(await validate())) {
-        return false;
-      }
-      await Remote.showLoading(() => task());
-    } on AppException catch (e, s) {
-      error = e.msg;
-      log(s.toString());
-    } on ExpiredToken catch (_) {
-      if (retry) {
-        try {
-          error = await Remote.showLoading(
-            () => Remote.renewToken(),
-          );
-          if (error == null) {
-            return runTask(retry: false);
-          }
-        } on LogOutUser catch (_) {
-          Get.back();
-          await Remote.logOut();
+    return _mutex.protect(() async {
+      String? error;
+      try {
+        if (!(await validate())) {
           return false;
-        } catch (_) {
+        }
+        await Remote.showLoading(() => task());
+      } on AppException catch (e, s) {
+        error = e.msg;
+        log(s.toString());
+      } on ExpiredToken catch (_) {
+        if (retry) {
+          try {
+            error = await Remote.showLoading(
+              () => Remote.renewToken(),
+            );
+            if (error == null) {
+              return runTask(retry: false);
+            }
+          } on LogOutUser catch (_) {
+            Get.back();
+            await Remote.logOut();
+            return false;
+          } catch (_) {
+            error = "Something went wrong";
+          }
+        }
+        error = 'Failed to authenticate user';
+      } catch (e, s) {
+        if (Remote.customErrorHandler.containsKey(e.runtimeType)) {
+          error = Remote.customErrorHandler[e.runtimeType]!(e);
+        } else {
+          Remote.logError(e, s);
+          log(e.toString());
+          log(s.toString());
           error = "Something went wrong";
         }
       }
-      error = 'Failed to authenticate user';
-    } catch (e, s) {
-      if (Remote.customErrorHandler.containsKey(e.runtimeType)) {
-        error = Remote.customErrorHandler[e.runtimeType]!(e);
-      } else {
-        Remote.logError(e, s);
-        log(e.toString());
-        log(s.toString());
-        error = "Something went wrong";
-      }
-    }
 
-    if (error != null) {
-      Remote.showError(message: error, confirmText: "Retry", callback: runTask);
-      _mutex.release();
-      return false;
-    } else {
-      onDone();
-      _mutex.release();
-      return true;
-    }
+      if (error != null) {
+        Remote.showError(message: error, confirmText: "Retry", callback: runTask);
+        return false;
+      } else {
+        onDone();
+        return true;
+      }
+    });
   }
 }
 
